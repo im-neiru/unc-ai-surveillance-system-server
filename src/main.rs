d64e6ffc6fdffa253c658ffb8a92eba421c95fae
neilcantorne <cantorne.neil@gmail.com>
2022-10-04 20:55:51 +0800

    add tokio and actix_web test code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-async fn main() -> std::io::Result<()> {
+use actix_web::{HttpServer, App, Responder, HttpResponse};
+use tokio;
+
+#[actix_web::get("/")]
+async fn root() -> impl Responder {
+    HttpResponse::Ok().body("Test")
+}
+
+fn main() -> std::io::Result<()> {
+    tokio::runtime::Builder::new_current_thread()
+    .enable_all()
+    .build()
+    .unwrap()
+    .block_on(start_server())
+}
+
+async fn start_server() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
-            .service(greet)
+            .service(root)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
