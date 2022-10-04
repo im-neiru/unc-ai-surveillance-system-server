b69192a9ab8d5395be7590c3eb28386373f9b76f
neilcantorne <cantorne.neil@gmail.com>
2022-10-04 23:07:29 +0800

    add users migration

---
+++ migrations/2022-10-04-145119_create_users/up.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE user_role AS ENUM('security-guard', 'security-head');
+
+CREATE TABLE users(
+    id uuid DEFAULT uuid_generate_v4(),
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    password_hash CHAR(64) NOT NULL,
+    assigned_role user_role NOT NULL,
+
+    PRIMARY KEY (id)
+);
