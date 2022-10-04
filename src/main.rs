4f314978ac7d6d9223385af1b52b750a231bd471
neilcantorne <cantorne.neil@gmail.com>
2022-10-04 21:47:13 +0800

    integrate ServerConfig

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
 use actix_web::{HttpServer, App, Responder, HttpResponse};
 use tokio;
 
+// local imports
+mod server_config;
+
+use server_config::ServerConfig;
+
 #[actix_web::get("/")]
 async fn root() -> impl Responder {
     HttpResponse::Ok().body("Test")
 }
 
 fn main() -> std::io::Result<()> {
+    let server_config = ServerConfig::load();
+
     tokio::runtime::Builder::new_current_thread()
     .enable_all()
     .build()
     .unwrap()
-    .block_on(start_server())
+    .block_on(start_server(&server_config))
 }
 
-async fn start_server() -> std::io::Result<()> {
+async fn start_server(server_config: &ServerConfig) -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(root)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(server_config.web_server.clone())?
     .run()
     .await
 }
