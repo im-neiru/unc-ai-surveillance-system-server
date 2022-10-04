cc0eaa9faa34b16007cafc59b3fa103e080246e9
neilcantorne <cantorne.neil@gmail.com>
2022-10-04 20:25:42 +0800

    add essential dependencies
    tokio = "1.21.0"
    actix-web = "4"
    serde = "1.0"
    serde_yaml = "0.9"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
