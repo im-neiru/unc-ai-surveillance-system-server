d984b352375648516543ea5c4ff986c2cdbe4703
neilcantorne <cantorne.neil@gmail.com>
2022-10-07 20:37:46 +0800

    update schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,20 +1,12 @@
 // @generated automatically by Diesel CLI.
 
-pub mod sql_types {
-    #[derive(diesel::sql_types::SqlType)]
-    #[diesel(postgres_type(name = "user_role"))]
-    pub struct UserRole;
-}
-
 diesel::table! {
-    use diesel::sql_types::*;
-    use super::sql_types::UserRole;
-
     users (id) {
         id -> Uuid,
+        username -> Varchar,
         first_name -> Varchar,
         last_name -> Varchar,
-        password_hash -> Bpchar,
-        assigned_role -> UserRole,
+        password_hash -> Bytea,
+        assigned_role -> Int2,
     }
 }
