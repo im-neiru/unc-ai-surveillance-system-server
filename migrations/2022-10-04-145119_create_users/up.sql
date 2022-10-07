129e410b3929b784d31f363cd87e76e3e7635556
neilcantorne <cantorne.neil@gmail.com>
2022-10-07 20:04:00 +0800

    replace user role with numeric number

--- migrations/2022-10-04-145119_create_users/up.sql
+++ migrations/2022-10-04-145119_create_users/up.sql
@@ -1,14 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TYPE user_role AS ENUM('security-guard', 'security-head');
-
 CREATE TABLE users(
     id uuid DEFAULT uuid_generate_v4(),
     username VARCHAR(24) NOT NULL,
     first_name VARCHAR(80) NOT NULL,
     last_name VARCHAR(80) NOT NULL,
     password_hash BYTEA NOT NULL,
-    assigned_role user_role NOT NULL,
+    assigned_role SMALLINT NOT NULL CHECK(assigned_role IN (1, 2, 3)),
 
     PRIMARY KEY (id)
 );
