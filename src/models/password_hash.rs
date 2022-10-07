7eef2c35213df17a4f123acb183fd0acd000e818
neilcantorne <cantorne.neil@gmail.com>
2022-10-08 05:08:58 +0800

    implement diesel deserialization

--- src/models/password_hash.rs
+++ src/models/password_hash.rs
@@ -3,6 +3,7 @@ use std::fmt::Display;
 use diesel::AsExpression;
 use diesel::pg::Pg;
 use diesel::serialize::ToSql;
+use diesel::deserialize::FromSql;
 use diesel::sql_types::Binary;
 
 #[derive(Debug, Clone)]
@@ -26,5 +27,15 @@ impl Display for PasswordHash {
 impl<'a> ToSql<Binary, Pg> for PasswordHash where &'a [u8]: ToSql<Binary, Pg> {
     fn to_sql<'b>(&'b self, out: &mut diesel::serialize::Output<'b, '_, Pg>) -> diesel::serialize::Result {
         <[u8] as ToSql<Binary, Pg>>::to_sql(self.0.as_ref(), out)
+    }
+}
+
+impl FromSql<Binary, Pg> for PasswordHash where Vec<u8>: FromSql<Binary, Pg> {
+    fn from_sql(bytes: diesel::backend::RawValue<'_, Pg>) -> diesel::deserialize::Result<Self> {
+        if let Ok(arr) = <Vec<u8> as FromSql<Binary, Pg>>::from_sql(bytes)?.try_into() {
+            return Ok(Self(arr));
+        }
+
+        Err("Cannot convert to PasswordHash".into())
     }
 }
