a59548047ac73437e392c1e81ce4efb8c4b6e87c
neilcantorne <cantorne.neil@gmail.com>
2022-10-07 22:29:53 +0800

    implement sql type serializiation

--- src/models/user_role.rs
+++ src/models/user_role.rs
@@ -1,6 +1,25 @@
+use diesel::backend::Backend;
+use diesel::expression::AsExpression;
+use diesel::serialize::ToSql;
+use diesel::sql_types::SmallInt;
+
 #[derive(Debug, Clone, Copy)]
+#[derive(AsExpression)]
+#[diesel(sql_type = SmallInt)]
 pub enum UserRole {
     SecurityGuard,
     SecurityHead,
     SystemAdmin,
+}
+
+const NUMERIC_VALUES : [u16; 3] = [1, 2, 3];
+
+impl<DB> ToSql<SmallInt, DB> for UserRole where DB: Backend, u16: ToSql<SmallInt, DB> {
+    fn to_sql<'b>(&'b self, out: &mut diesel::serialize::Output<'b, '_, DB>) -> diesel::serialize::Result {
+        (match self {
+            UserRole::SecurityGuard => &NUMERIC_VALUES[0],
+            UserRole::SecurityHead => &NUMERIC_VALUES[1],
+            UserRole::SystemAdmin => &NUMERIC_VALUES[2],
+        }).to_sql(out)
+    }
 }
