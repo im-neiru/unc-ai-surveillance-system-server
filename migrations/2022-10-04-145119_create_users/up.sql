775912ac71f8ba24c9fb107bdb4a4fded42bb3e1
neilcantorne <cantorne.neil@gmail.com>
2022-10-07 19:42:34 +0800

    add username

--- migrations/2022-10-04-145119_create_users/up.sql
+++ migrations/2022-10-04-145119_create_users/up.sql
@@ -4,6 +4,7 @@ CREATE TYPE user_role AS ENUM('security-guard', 'security-head');
 
 CREATE TABLE users(
     id uuid DEFAULT uuid_generate_v4(),
+    username VARCHAR(24) NOT NULL,
     first_name VARCHAR(80) NOT NULL,
     last_name VARCHAR(80) NOT NULL,
     password_hash BYTEA NOT NULL,
