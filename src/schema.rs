ec3cb70412157ae3909f9a743952cf2f82dbe875
neilcantorne <cantorne.neil@gmail.com>
2022-10-08 17:27:36 +0800

    update schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,19 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    sessions (id) {
+        id -> Uuid,
+        user_id -> Uuid,
+        created_time -> Timestamp,
+        last_login -> Timestamp,
+        logout_time -> Nullable<Timestamp>,
+        device_type -> Int2,
+        device_name -> Varchar,
+        device_sig -> Bytea,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Uuid,
         username -> Varchar,
@@ -10,3 +23,10 @@ diesel::table! {
         assigned_role -> Int2,
     }
 }
+
+diesel::joinable!(sessions -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    sessions,
+    users,
+);
