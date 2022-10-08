84e26ce97ba87b2003c9ef3378396ae96ec860cf
neilcantorne <cantorne.neil@gmail.com>
2022-10-09 00:45:02 +0800

    include device os

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,9 +1,11 @@
 mod user_role;
 mod password_hash;
+mod device_os;
 mod user;
 
 pub use user_role::UserRole;
 pub use password_hash::PasswordHash;
+pub use device_os::DeviceOs;
 
 pub use user::UserInsert;
 pub use user::UserSelect;
