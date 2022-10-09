e494c37e48c4ad225e41ccb08503c9514a963e7d
neilcantorne <cantorne.neil@gmail.com>
2022-10-09 21:54:05 +0800

    create session model

---
+++ src/models/session.rs
@@ -0,0 +1,15 @@
+use diesel::Insertable;
+use super::DeviceOs;
+
+use chrono::NaiveDateTime;
+
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::sessions)]
+pub struct SessionInsert<'a> {
+    user_id: uuid::Uuid,
+    created_time: NaiveDateTime,
+    last_login: NaiveDateTime,
+    logout_time: Option<NaiveDateTime>,
+    device_os: DeviceOs,
+    device_name: &'a str,
+}
