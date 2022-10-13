89635ae13263e4655cefda943a5bb5be88a67c5c
neilcantorne <cantorne.neil@gmail.com>
2022-10-14 05:49:42 +0800

    impl fn create

--- src/models/session.rs
+++ src/models/session.rs
@@ -14,3 +14,22 @@ pub struct SessionInsert<'a> {
     device_name: &'a str,
     device_hash: &'a [u8]
 }
+
+impl SessionInsert<'_> {
+    pub fn create(user_id: uuid::Uuid,
+                    device_os: DeviceOs,
+                    device_name: &str,
+                    device_hash: &[u8]) -> Self {
+        let now = chrono::Utc::now().naive_utc();
+
+        Self {
+            user_id,
+            created_time: now,
+            last_login: now,
+            logout_time: None,
+            device_os,
+            device_name,
+            device_hash,
+        }
+    }
+}
