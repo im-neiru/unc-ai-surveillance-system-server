ba588b39265072030168755b5acf58597b58bf8a
neilcantorne <cantorne.neil@gmail.com>
2022-10-14 01:29:28 +0800

    test from hexadecimal

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,14 @@
 use crate::traits::ToHexadecimal;
+use crate::traits::FromHexadecimal;
 
 #[test]
 fn test_hexadecimal() {
     assert!(0x60A344u128.to_hexadecimal() == "0000000000000000000000000060A344",
-        "Erroneous hexadecimal conversion");
+        "Erroneous conversion to hexadecimal");
+
+    assert!(u128::from_hexadecimal("0000000000000000000000000060A344").unwrap() == 0x60A344,
+    "Erroneous conversion from hexadecimal");
+
+    assert!(u128::from_hexadecimal("A00000060A344").unwrap() == 0xA00000060A344,
+    "Erroneous conversion from hexadecimal");
 }
