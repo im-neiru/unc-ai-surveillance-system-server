a2717f84b9970f94112fae19db500db53d0cde75
neilcantorne <cantorne.neil@gmail.com>
2022-10-15 03:35:28 +0800

    login json web token and session creation

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,13 +1,14 @@
 use actix_web::{post, web};
 use actix_web::{HttpResponse, Responder};
 
-use diesel::{QueryDsl, RunQueryDsl, OptionalExtension};
+use diesel::r2d2::{PooledConnection, ConnectionManager};
+use diesel::{QueryDsl, RunQueryDsl, OptionalExtension, PgConnection};
 use diesel::ExpressionMethods;
 
 use serde::{Serialize, Deserialize};
 
 use crate::data::AppData;
-use crate::models::{UserSelect, DeviceSignature, DeviceOs};
+use crate::models::{UserSelect, DeviceSignature, DeviceOs, Claims, SessionInsert};
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 struct LoginData {
     pub username: String,
@@ -17,28 +18,55 @@ struct LoginData {
     pub device_signature: DeviceSignature,
 }
 
-
-
 #[post("/login")]
 async fn post_login((body, state): 
-    (web::Json<LoginData>, web::Data<AppData>)) -> impl Responder {
-        
+(web::Json<LoginData>, web::Data<AppData>)) -> impl Responder {
     use crate::schema::users::dsl::*;
     
     let argon2 = state.argon2(&body.password);
     let mut database = state.connect_database();
+    let jwt;
 
     if let Some(user) = users.filter(username.eq(&body.username)).first::<UserSelect>(&mut database).optional().unwrap()
     {
-        if argon2 == user.password_hash {
-            todo!("Generate web tokens");
+        if argon2 != user.password_hash {
+            return HttpResponse::Unauthorized().body("");
         }
+
+        jwt = create_session(state, &mut database, &body, user).await;
     }
     else {
-        return HttpResponse::Unauthorized();
+        return HttpResponse::Unauthorized().body("");
     }
 
-    HttpResponse::Ok()
+    HttpResponse::Ok().body(jwt)
+}
+
+async fn create_session(state: web::Data<AppData>,
+    database: &mut PooledConnection<ConnectionManager<PgConnection>>,
+    login_data: &LoginData,
+    user: UserSelect) -> String {
+
+
+    let dev_hash = state.xxh3_128bits(login_data.device_signature.into()).await.to_ne_bytes();
+    
+    let session_id = {
+        use crate::schema::sessions::dsl::*;
+        let record = SessionInsert::create(
+            &user.id,
+            &login_data.device_os,
+            &login_data.device_name,
+            &dev_hash,
+        );
+
+        diesel::insert_into(sessions)
+        .values(&record)
+        .returning(id)
+        .get_result::<uuid::Uuid>(&mut *database)
+        .unwrap()
+    };
+
+    state.jwt_encode(&Claims { session_id })
 }
 
 pub fn scope() -> actix_web::Scope {
