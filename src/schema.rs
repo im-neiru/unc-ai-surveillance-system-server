c1843373b47f4d5c0e4e8ee66510e1dd8af62eff
neilcantorne <cantorne.neil@gmail.com>
2022-10-21 02:10:57 +0800

    update schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,32 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    area (code) {
+        code -> Varchar,
+        name -> Varchar,
+    }
+}
+
+diesel::table! {
+    protocol_violations (id) {
+        id -> Uuid,
+        personnel_id -> Uuid,
+        date_time -> Timestamp,
+        area_code -> Varchar,
+        category -> Int2,
+    }
+}
+
+diesel::table! {
+    protocol_violators (id) {
+        id -> Uuid,
+        first_name -> Varchar,
+        last_name -> Varchar,
+        category -> Varchar,
+    }
+}
+
+diesel::table! {
     sessions (id) {
         id -> Uuid,
         user_id -> Uuid,
@@ -24,9 +50,14 @@ diesel::table! {
     }
 }
 
+diesel::joinable!(protocol_violations -> area (area_code));
+diesel::joinable!(protocol_violations -> users (personnel_id));
 diesel::joinable!(sessions -> users (user_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
+    area,
+    protocol_violations,
+    protocol_violators,
     sessions,
     users,
 );
