b5bf2a32f3c00bd10ee2c6d22e0ae57a49539e17
neilcantorne <cantorne.neil@gmail.com>
2022-10-21 13:26:26 +0800

    impl Loggable and LoggableWithResponse

--- src/logging/loggable_error.rs
+++ src/logging/loggable_error.rs
@@ -1,3 +1,7 @@
+use std::borrow::Borrow;
+
+use actix_web::{Responder, CustomizeResponder, http::StatusCode};
+
 pub struct Loggable {
     pub message: String
 }
@@ -5,4 +9,27 @@ pub struct Loggable {
 pub struct LoggableWithResponse {
     pub message: String,
     pub status_code: actix_web::http::StatusCode,
+}
+
+impl Loggable {
+    pub fn log(&self, writer: &mut super::LogWriter) {
+        writer.write(&self.message);
+    }
+
+    pub fn with_response(&self, status_code: StatusCode) -> LoggableWithResponse {
+        LoggableWithResponse {
+            message: self.message.clone(),
+            status_code }
+    }
+}
+
+impl LoggableWithResponse {
+    pub fn log(&self, writer: &mut super::LogWriter) -> CustomizeResponder<String> {
+        writer.write(&self.message);
+
+        self.message
+            .clone()
+            .customize()
+            .with_status(self.status_code)
+    }
 }
