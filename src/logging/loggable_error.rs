f3bba4b8a14743c0382de891ddcc25a2e9689294
neilcantorne <cantorne.neil@gmail.com>
2022-10-24 06:44:44 +0800

    split message to log and response

--- src/logging/loggable_error.rs
+++ src/logging/loggable_error.rs
@@ -1,29 +1,32 @@
 use std::fmt::Display;
 
 use actix_web::{Responder, CustomizeResponder, http::StatusCode};
+use serde_json::json;
 
 #[derive(Clone, Debug)]
 pub struct Loggable {
-    pub message: String,
+    pub log_message: String,
     timestamp: chrono::DateTime<chrono::Utc>
 }
 
 
 #[derive(Clone, Debug)]
 pub struct LoggableWithResponse {
-    pub message: String,
+    pub log_message: String,
+    pub response_message: String,
     pub status_code: actix_web::http::StatusCode,
     timestamp: chrono::DateTime<chrono::Utc>
 }
 
 impl Loggable {
     pub async fn log<const LEVEL: super::LogLevel>(&self, writer: &mut super::LogWriter<LEVEL>) {
-        writer.write(&self.message, self.timestamp).await;
+        writer.write(&self.log_message, self.timestamp).await;
     }
 
-    pub fn with_response(&self, status_code: StatusCode) -> LoggableWithResponse {
+    pub fn with_response(&self, status_code: StatusCode, response_message: Option<&str>) -> LoggableWithResponse {
         LoggableWithResponse {
-            message: self.message.clone(),
+            log_message: self.log_message.clone(),
+            response_message: response_message.unwrap_or(&self.log_message).to_string(),
             timestamp: self.timestamp,
             status_code
         }
@@ -32,10 +35,10 @@ impl Loggable {
 
 impl LoggableWithResponse {
     pub async fn log<const LEVEL: super::LogLevel>(&self, writer: &mut super::LogWriter<LEVEL>) -> CustomizeResponder<String> {
-        writer.write(&self.message, self.timestamp).await;
+        writer.write(&self.log_message, self.timestamp).await;
 
-        self.message
-            .clone()
+        json!({ "message": self.response_message })
+            .to_string()
             .customize()
             .with_status(self.status_code)
     }
@@ -43,12 +46,12 @@ impl LoggableWithResponse {
 
 impl Display for Loggable {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.message)
+        f.write_str(&self.log_message)
     }
 }
 
 impl Display for LoggableWithResponse {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.message)
+        f.write_str(&self.log_message)
     }
 }
