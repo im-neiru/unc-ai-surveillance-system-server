015f7a1c5700d5f1b9b355d9471f6d2ee166fcb1
neilcantorne <cantorne.neil@gmail.com>
2022-10-25 06:30:55 +0800

    remove loggable errors

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -8,18 +8,3 @@ pub use log_recorder::LogRecorder;
 pub use log_recorder::LogWriter;
 pub use log_recorder::LogLevel;
 
-pub use loggable_error::Loggable;
-pub use loggable_error::LoggableWithResponse;
-pub use loggable_error::LogResponseError;
-
-pub type LoggedResult<T> = Result<T, LogResponseError>;
-
-#[macro_export]
-macro_rules! try_log {
-    ($res:expr, $logger:expr) => {
-        match ($res) {
-            Ok(obj) => obj,
-            Err(err) => return Ok(err.log($logger).await?),
-        }
-    };
-}
