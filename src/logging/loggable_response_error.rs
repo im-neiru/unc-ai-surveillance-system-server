c862e1eddcf5e93cffefa9f3e54246e535971b99
neilcantorne <cantorne.neil@gmail.com>
2022-10-25 08:14:52 +0800

    impl ResponseError for LoggableResponseError

--- src/logging/loggable_response_error.rs
+++ src/logging/loggable_response_error.rs
@@ -1,6 +1,7 @@
-use actix_web::http::StatusCode;
+use actix_web::http::{StatusCode, header::HeaderValue};
 use chrono::{DateTime, Utc};
 
+#[derive(Clone, Debug)]
 pub struct LoggableResponseError {
     pub(super) message: (String, String),
     pub(super) level: super::LogLevel,
@@ -20,5 +21,29 @@ impl super::Loggable for LoggableResponseError {
 
     fn timestamp(&self) -> DateTime<Utc> {
         self.timestamp
+    }
+}
+
+impl actix_web::ResponseError for LoggableResponseError {
+    fn status_code(&self) -> StatusCode {
+        self.status_code
+    }
+
+    fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
+        let mut res = actix_web::HttpResponse::new(self.status_code());
+
+        res.headers_mut()
+            .insert(actix_web::http::header::CONTENT_TYPE, 
+                HeaderValue::from_static("application/json"));
+
+        res.set_body(actix_web::body::BoxBody::new(
+            format!("{{\"message\": {}}}", self.message.1)
+        ))
+    }
+}
+
+impl std::fmt::Display for LoggableResponseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.message.0)
     }
 }
