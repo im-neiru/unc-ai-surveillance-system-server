77f897898f66d9162a3ca2f3e956e08e97ca1e61
neilcantorne <cantorne.neil@gmail.com>
2022-10-25 09:22:34 +0800

    integrate logging mechanism

--- src/models/user_claims.rs
+++ src/models/user_claims.rs
@@ -1,13 +1,15 @@
 use std::pin::Pin;
 use std::future::Future;
 
+use actix_web::http::StatusCode;
 use actix_web::web::Data;
-use actix_web::{HttpRequest};
+use actix_web::HttpRequest;
 use actix_web::dev::Payload;
 
 use diesel::{Queryable, AsChangeset, QueryDsl, RunQueryDsl, ExpressionMethods};
 
 use crate::data::AppData;
+use crate::logging::{LogLevel, LoggableResponseError};
 
 #[derive(Debug, Queryable, AsChangeset)]
 #[diesel(table_name = crate::schema::users)]
@@ -17,21 +19,26 @@ pub struct UserClaims {
 }
 
 impl actix_web::FromRequest for UserClaims {
-    type Error = actix_web::Error;
+    type Error = LoggableResponseError;
 
-    type Future = Pin<Box<dyn Future<Output = Result<Self, actix_web::Error>>>>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, LoggableResponseError>>>>;
 
     fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
         let req = req.clone();
         
         Box::pin(async move {
             let token = req.cookie("jwt")
-                .unwrap()
+                .ok_or(LoggableResponseError::new(
+                    "JSON Web token not found",
+                    "Invalid Session",
+                    LogLevel::Information,
+                    StatusCode::UNAUTHORIZED)
+                )?
                 .value()
                 .to_owned();
             
             let state = req.app_data::<Data<AppData>>().unwrap();
-            let jwtc = state.jwt_decode(&token);
+            let jwtc = state.jwt_decode(&token)?;
 
             use crate::schema::sessions;
             use crate::schema::users;
