13360a774b916c5f2319c350c43cf1c803bf1887
neilcantorne <cantorne.neil@gmail.com>
2022-10-25 08:35:03 +0800

    add ServerError

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -3,9 +3,11 @@ mod loggable;
 mod logger;
 mod log_level;
 mod loggable_response_error;
+mod server_error;
 
 pub use logger::Logger;
 pub use log_level::LogLevel;
 pub use loggable::Loggable;
 pub use log_recorder::LogRecorder;
 pub use loggable_response_error::LoggableResponseError;
+pub use server_error::ServerError;
