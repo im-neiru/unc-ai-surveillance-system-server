a284839f9cca0014cdf8513b4e2f7b36234fd218
neilcantorne <cantorne.neil@gmail.com>
2023-01-05 21:03:05 +0800

    implement thread safety for Camera

--- src/media/camera.rs
+++ src/media/camera.rs
@@ -7,10 +7,11 @@ use opencv::{
     },
     prelude::VideoCaptureTrait,
 };
+use tokio::sync::Mutex;
 
 use crate::logging::LogResult;
 
-pub struct Camera(VideoCapture);
+pub struct Camera(Mutex<VideoCapture>);
 
 pub struct CameraReader<'a,> {
     camera: &'a mut Camera,
@@ -23,9 +24,15 @@ impl Camera {
         source.new_camera()
     }
 
-    pub fn begin(&mut self) -> LogResult<CameraReader> {
-        let w = self.0.get(CAP_PROP_FRAME_WIDTH)? as u32;
-        let h = self.0.get(CAP_PROP_FRAME_HEIGHT)? as u32;
+    pub async fn begin(&mut self) -> LogResult<CameraReader> {
+        let w; let h;
+
+        {
+            let vc = self.0.lock().await;
+
+            w = vc.get(CAP_PROP_FRAME_WIDTH)? as u32;
+            h = vc.get(CAP_PROP_FRAME_HEIGHT)? as u32;
+        }
 
         Ok(CameraReader {
             camera: self,
@@ -40,31 +47,33 @@ pub trait CameraSource {
 
 impl CameraSource for &str {
     fn new_camera(&self) -> crate::logging::LogResult<Camera> {
-        Ok(Camera(VideoCapture::from_file(
+        Ok(Camera(Mutex::const_new(VideoCapture::from_file(
             self,
-            opencv::videoio::CAP_ANY)?))
+            opencv::videoio::CAP_ANY)?)))
     }
 }
 
 impl CameraSource for String {
     fn new_camera(&self) -> LogResult<Camera> {
-        Ok(Camera(VideoCapture::from_file(
+        Ok(Camera(Mutex::const_new(VideoCapture::from_file(
             self,
-            opencv::videoio::CAP_ANY)?))
+            opencv::videoio::CAP_ANY)?)))
     }
 }
 
 impl CameraSource for u32 {
     fn new_camera(&self) -> LogResult<Camera> {
-        Ok(Camera(VideoCapture::new(
+        Ok(Camera(Mutex::const_new(VideoCapture::new(
             *self as i32,
-            opencv::videoio::CAP_ANY)?))
+            opencv::videoio::CAP_ANY)?)))
     }
 }
 
 impl<'a, 'b> CameraReader<'a> {
-    pub fn next(&'b mut self) -> Option<LogResult<&'b super::Frame>> {
-        match self.camera.0.read(&mut self.buffer) {
+    pub async fn next(&'b mut self) -> Option<LogResult<&'b super::Frame>> {
+        let mut vc = self.camera.0.lock().await;
+
+        match vc.read(&mut self.buffer) {
             Ok(exists) => if exists { Some(Ok(&self.buffer)) }
             else { None },
             Err(error) => Some(Err(error.into())),
