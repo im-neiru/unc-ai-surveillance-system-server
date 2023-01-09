c0b4c5b02349df93ca456ab026be83455bd02656
neilcantorne <cantorne.com@gmail.com>
2023-01-09 08:33:44 +0800

    address warning

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,4 +2,4 @@ pub(crate) mod users;
 pub(crate) mod logs;
 pub(crate) mod vstream;
 
-type Result<T, E: actix_web::ResponseError = crate::logging::ResponseError> = std::result::Result<T, E>;
+type Result<T, E = crate::logging::ResponseError> = std::result::Result<T, E>;
