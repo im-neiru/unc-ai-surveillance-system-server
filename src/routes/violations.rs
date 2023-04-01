862f9d183ebc7d31bbe2eaed047a700d6292f43c
neilcantorne <cantorne.com@gmail.com>
2023-04-01 23:26:31 +0800

    implement /identified

--- src/routes/violations.rs
+++ src/routes/violations.rs
@@ -1,5 +1,5 @@
 use crate::logging::LogLevel;
-use crate::models::{Category, UserRole, ViolationUnknown};
+use crate::models::{Category, IdentifiedViolation, UserRole, ViolationUnknown};
 use crate::{data::AppData, models::UserClaims};
 use actix_web::http::StatusCode;
 use actix_web::{web, HttpResponse, Responder};
@@ -42,6 +42,66 @@ async fn get_unidentified(
             .get_results::<ViolationUnknown>(&mut connection)
             .unwrap();
     }
+
+    Ok(serde_json::to_string(&list)
+        .unwrap()
+        .customize()
+        .with_status(StatusCode::OK))
+}
+
+#[derive(Deserialize)]
+struct GetIdentifiedQuery {
+    #[serde(alias = "area-code")]
+    area_code: Option<String>,
+}
+
+#[actix_web::get("/identified")]
+async fn get_identified(
+    (state, user, query): (
+        web::Data<AppData>,
+        UserClaims,
+        web::Query<GetIdentifiedQuery>,
+    ),
+) -> super::Result<impl Responder> {
+    use crate::schema::violations::dsl::*;
+
+    let mut connection = state.connect_database();
+
+    if user.assigned_role == UserRole::SecurityGuard {}
+
+    let list = match query.area_code.clone() {
+        Some(area) => violations
+            .filter(identified.eq(true))
+            .filter(area_code.eq(area))
+            .order_by(date_time)
+            .select((
+                id,
+                area_code,
+                violation_kind,
+                date_time,
+                personnel_id,
+                first_name,
+                last_name,
+                category,
+            ))
+            .get_results::<IdentifiedViolation>(&mut connection)
+            .unwrap(),
+        None => violations
+            .filter(identified.eq(true))
+            .order_by(date_time)
+            .select((
+                id,
+                area_code,
+                violation_kind,
+                date_time,
+                personnel_id,
+                first_name,
+                last_name,
+                category,
+            ))
+            .get_results::<IdentifiedViolation>(&mut connection)
+            .unwrap(),
+    };
 
     Ok(serde_json::to_string(&list)
         .unwrap()
@@ -105,15 +165,16 @@ async fn patch_record(
 
     let mut connection = state.connect_database();
 
-    diesel::update(violations::table.filter(violations::id.eq(request.id))).set((
-        violations::personnel_id.eq(Some(user.id)),
-        violations::first_name.eq(Some(&request.first_name)),
-        violations::last_name.eq(Some(&request.last_name)),
-        violations::category.eq(Some(&request.category)),
-        violations::identified.eq(true),
-    ))
-    .execute(&mut connection)
-    .unwrap();
+    diesel::update(violations::table.filter(violations::id.eq(request.id)))
+        .set((
+            violations::personnel_id.eq(Some(user.id)),
+            violations::first_name.eq(Some(&request.first_name)),
+            violations::last_name.eq(Some(&request.last_name)),
+            violations::category.eq(Some(&request.category)),
+            violations::identified.eq(true),
+        ))
+        .execute(&mut connection)
+        .unwrap();
 
     Ok(HttpResponse::build(StatusCode::OK))
 }
@@ -121,6 +182,7 @@ async fn patch_record(
 pub fn scope() -> actix_web::Scope {
     web::scope("/violations")
         .service(get_unidentified)
+        .service(get_identified)
         .service(get_image)
         .service(patch_record)
 }
